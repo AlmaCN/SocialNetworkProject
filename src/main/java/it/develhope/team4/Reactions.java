97878d6ddbbfd28b6d6c9e1760eb05a9ddba10a9
Flavio <flaviopagano1@gmail.com>
2023-02-06 16:15:20 +0100

    Testing Class and Reactions class created

---
+++ src/main/java/it/develhope/team4/Reactions.java
@@ -0,0 +1,21 @@
+package it.develhope.team4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reactions {
+
+    /**
+     * Come creare le reactions? sono degli oggetti?
+     * Come ottenere partendo da un oggetto "Like" un print che informa
+     * che l'oggetto sia stato usato?
+     */
+
+    public void reactions(){
+        ArrayList <String> myList = new ArrayList<>();
+        myList.add("like");
+        myList.add("hug");
+        myList.add("angry");
+
+    }
+}
---
+++ src/main/java/it/develhope/team4/Test.java
@@ -0,0 +1,44 @@
+package it.develhope.team4;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("Enter text here");
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String post = scanner.next();
+
+        LocalDateTime now = LocalDateTime.now();
+
+        switch (post) {
+            case "like" -> {
+                System.out.println("Used: Like reaction");
+                System.out.println("Post successfully created");
+                System.out.println(dtf.format(now));
+            }
+            case "hug" -> {
+                System.out.println("Used: Hug reaction");
+                System.out.println("Post successfully created");
+                System.out.println(dtf.format(now));
+            }
+            case "angry" -> {
+                System.out.println("Used: Angry reaction");
+                System.out.println("Post successfully created");
+                System.out.println(dtf.format(now));
+            }
+            default -> {
+                System.out.println("No reaction used");
+                System.out.println("Post successfully created");
+                System.out.println(dtf.format(now));
+            }
+        }
+    }
+}
