41e5dcaacb13bde0b6c7d1008caa7b134a3b2865
ale <alessandrogbt94@gmail.com>
2023-02-12 18:02:38 +0100

    primo commit domenica pomeriggio

--- src/main/java/it/develhope/team4/entities/User.java
+++ src/main/java/it/develhope/team4/entities/User.java
@@ -39,35 +39,69 @@ public class User {
     public static User register(String name, String surname, LocalDate birthDate, String email, String username, String telephone, String password){
         return new User(name, surname, birthDate, email, username, telephone, password);
     }
+
+
     public static void loginUsername(String username, String password, boolean twoFactorsAuthentication){
+        /**Mock loginQuery*/
+        /*
+        try (Connection con = DriverManager.getConnection(url, user, password)) {
+            Statement statement = con.createStatement();
+            String query = "Select DBpassword from DBuserName";
+            if(password.equals(DBpassword){
+            //fai continuare il login process
+            }else{
+            //restituisci un errore
+            }
+        }catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+         */
+
         //nella tabella del database deve cercare nella colonna username se questo esiste
         //se esiste prendi l'id dell'user e vedi se la password di quell'id corrisponde
         //deve gestire se username non c'è e se password sbagliata
         //come usare la twoFactorsAuthentication
     }
+
     public static void loginEmail(String email, String password, boolean twoFactorsAuthentication){
         //lo stesso che con loginUsername
     }
+
     public static void loginTelephone(String telephone, String password, boolean twoFactorsAuthentication){
         //lo stesso che con loginUsername
     }
+
+
+
+
     public Post createPost(String postDescription, String urlImg, PrivacyType privacy){
         userPosts.add(new Post(this.userId, urlImg, postDescription, privacy));
         return new Post(this.userId, urlImg, postDescription, privacy);
     }
+
+    /** */
     public void requestFriendship(String userId){
+        //IO clicco il tasto che invia questa richiesta, quindi invio il mio ID?
+
         //implementazione?
     }
-
-    public void acceptFriend(String userId){
+    /** */
+    public void acceptFriend(String friendThatAskedToBeFriendUserId){
+        friends.add(friendThatAskedToBeFriendUserId);
         //implementazione?
     }
-
+    /** */
     public void declineFriend(){
         //implementazione?
+        //solo un messaggio di declinazione della richiesta? si puo' gestire solo frontend credo
     }
-    public void addFriend(String idFriend){
-        friends.add(idFriend);
+
+
+    /** */
+    //io clicco tasto "addFriend"
+    public void addFriend(String MyidFriend){
+        //mando il mio id al server o viceversa? Cosa mettere come parametro?
+        //
     }
     public void removeFriend(String idFriend){
         friends.remove(idFriend);
